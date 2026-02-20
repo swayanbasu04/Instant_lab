c718334e572d199f61bef8d95d23cc50164b91db
Swayan Basu <207083552+swayanbasu04@users.noreply.github.com>
2026-02-20 15:33:40 +0000

    Add files via upload

---
+++ start_parrot.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+sudo parrot-upgrade
+sudo apt update && sudo apt install -y docker.io
+mkdir -p "$PWD/work" "$PWD/msf"
+# Docker lab using Parrot enviroment
+docker run --rm -ti --network host -v "$PWD/work":/work parrotsec/security
+# install neccesary packages
+apt install wget net-tools python3 python3-pip wget curl hashcat john hydra git dig nikto metasploit-framework zapproxy dnsmap recon-ng sslscan gobuster 
+# Run nmap (uncomment to use)
+# docker run --rm -ti parrotsec/nmap options
+
+# Run metasploit container with mounted msf dir (uncomment to use)
+# docker run --rm -ti --network host -v $PWD/msf:/root/ parrotsec/metasploit
+docker run --rm -d --name juice-shop -p 3000:3000 bkimminich/juice-shop
+
